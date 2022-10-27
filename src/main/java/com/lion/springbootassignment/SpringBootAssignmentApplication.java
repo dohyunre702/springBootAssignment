789ca232dbfe55ea63760508966c34d40756e0c3
dohyu <dohyunre702@gmail.com>
2022-10-27 17:37:38 +0900

    First commit

---
+++ src/main/java/com/lion/springbootassignment/SpringBootAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.lion.springbootassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lion/springbootassignment/SpringBootAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lion.springbootassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
